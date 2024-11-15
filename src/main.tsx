6e170ff1f71e56015b7261f9b0409d18ed8cc46a
yurisdevops <yurimachado.dev@gmail.com>
2024-11-15 18:46:56 -0300

    Alteraçãoes pedidas pelo vercel.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import route from "./Routes/routes";
 import { AuthProvider } from "./Context/AuthContext";
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
 import { Toaster } from "react-hot-toast"; // Importando o componente Toaster de react-hot-toast para notificações
 
 import "./index.css";
