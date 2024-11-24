782ec648df90b49cdfd0f612c9ddeac3b5ad91df
yurisdevops <yurimachado.dev@gmail.com>
2024-11-24 18:18:58 -0300

    Upadte icon html.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';  
+import react from '@vitejs/plugin-react';  
 
-export default defineConfig({
-  plugins: [react()],
-  base: './', 
-});
+export default defineConfig({  
+  plugins: [react()],  
+  base: '/', // Isso é importante para o roteamento  
+  build: {  
+    outDir: 'dist', // Assegura que o conteúdo do build irá para a pasta correta  
+  },  
+});  
